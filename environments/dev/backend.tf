1432fb75724ce650f8c835ea4d0eb3e43d7d6082
sharmaaakash170 <sharmaaakash170@gmail.com>
2025-05-19 16:52:58 +0530

    fix: format terraform files properly

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-cicd-aws-infra-s3-bucket"
-    key = "dev/terraform.tfstate"
+    key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,12 +1,12 @@
 module "s3" {
-  source = "../../modules/s3"
+  source       = "../../modules/s3"
   project_name = var.project_name
-  tags = var.tags
+  tags         = var.tags
 }
 
 module "ec2" {
-  source = "../../modules/ec2"
-  ami_id = var.ami_id
+  source        = "../../modules/ec2"
+  ami_id        = var.ami_id
   instance_type = var.instance_type
-  tags = var.tags
+  tags          = var.tags
 }
